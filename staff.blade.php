21a57b19b0c4ded27190faefaf0c0ad062425bc9
Barubal665 <89123787+Barubal665@users.noreply.github.com>
2022-05-10 16:17:21 +0800

    Add files via upload

---
+++ staff.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/main.css">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+  
+</head>
+<body>
+
+<nav class="sidebar close">
+  <header>
+    <div class="image-text">
+      <span class="image">
+        <img src="/css/images/pisopay-icon.png" alt="">
+      </span>
+        <div class="header-text">
+          <span class="title">PisoPay.Inc</span>
+        </div>
+      <div class="text header-text">
+        
+      </div>
+    </div>
+    <i class='bx bx-chevron-right toggle'></i>
+  </header>
+  <div class="menu-bar">
+    <div class="menu">
+    <li class="search-box">
+         <i class='bx bx-search icon'></i>
+          <input type="search" placeholder="Search...">
+        </li>
+      <ul class="menu-links">
+        <li class="nav-link">
+          <a href="#">
+          <i class='bx bxs-dashboard icon'></i>
+          <span class="text nav-text">Dashboard</span>
+          </a>
+        </li>
+        <li class="nav-link">
+          <a href="#">
+          <i class='bx bx-food-menu icon'></i>
+          <span class="text nav-text">Order List</span>
+          </a>
+        </li>
+        <li class="nav-link">
+          <a href="#">
+          <i class='bx bx-cabinet icon'></i>
+          <span class="text nav-text">Inventory</span>
+          </a>
+        </li>
+        <li class="nav-link">
+          <a href="#">
+          <i class='bx bx-cart icon'></i>
+          <span class="text nav-text">Product</span>
+          </a>
+        </li>
+        <li class="nav-link">
+          <a href="#">
+          <i class='bx bx-user icon'></i>
+          <span class="text nav-text">User</span>
+          </a>
+        </li>
+        
+      </ul>
+    </div>
+    <div class="bottom-content">
+      <li class="">
+        <a href="#">
+          <i class="bx bx-log-out icon"></i>
+          <span class="text nav-tex">Logout</span>
+        </a>
+      </li>
+      <li class="mode">
+          <div class="moon-sun">
+            <i class="bx bx-moon icon moon"></i>
+            <i class="bx bx-sun icon sun"></i>
+          </div>
+          <span class="mode-text text">Dark Mode</span>
+          <div class="toggle-switch">
+            <span class="switch"></span>
+          </div>
+        </li>
+    </div>
+  </div>
+</nav>
+<script src="/js/app.js"></script>
+</body>
+</html>
